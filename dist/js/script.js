c82c492042e87cc6b30f93f2670445e6c18176e3
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2023-12-23 19:54:48 +0300

    Complete hero section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,21 @@
-console.log("test");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+function toggleBurger() {
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector(".menu");
+
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("open");
+  });
+}
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+  toggleBurger();
+}
+
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
