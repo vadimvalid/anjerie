38e21b6e8190e9ba1ff1fb312db5a5c430c1af35
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2023-12-25 08:53:17 +0300

    Add animations

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,64 @@ function toggleBurger() {
 if (window.matchMedia("(max-width: 768px)").matches) {
   toggleBurger();
 }
+
+const textAnimations = () => {
+  const headers = document.querySelectorAll(".header-animated");
+  const options = {
+    root: null,
+    threshold: 0.1,
+  };
+
+  const headerObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const { target } = entry;
+
+        const titles = target.querySelectorAll("h1,h2");
+
+        const { delay = 0 } = target.dataset;
+
+        for (let i = 0; i < titles.length; i++) {
+          let titleElement = titles[i];
+
+          titleElement.style.animationName = "headerAnimation";
+          titleElement.style.animationDuration = `1s`;
+          titleElement.style.animationFillMode = "forwards";
+        }
+      }
+    });
+  }, options);
+
+  headers.forEach((header) => headerObserver.observe(header));
+};
+
+const blockAnimatons = () => {
+  const blocks = document.querySelectorAll(".items .item");
+
+  const objOptions = {
+    root: null,
+    threshold: 0.3,
+  };
+
+  const blockObserver = new IntersectionObserver(callBackFunction, objOptions);
+
+  blocks.forEach((block) => {
+    if (block) {
+      blockObserver.observe(block);
+    }
+  });
+
+  function callBackFunction(entries, observer) {
+    entries.forEach((entry) => {
+      const curBlockName = entry.target.getAttribute("class");
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("hidden");
+      } else {
+        entry.target.classList.add("hidden");
+      }
+    });
+  }
+};
+
+textAnimations();
+blockAnimatons();
